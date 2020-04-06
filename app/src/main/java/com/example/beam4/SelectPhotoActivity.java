a384ce39182ff8cb8b88d200fddce3fb9faefe68
Park-hyeji1023 <binoo1023@gmail.com>
2020-04-06 16:29:19 +0900

    사진 선택뷰 삭제된 것 다시 재구성

--- app/src/main/java/com/example/beam4/SelectPhotoActivity.java
+++ app/src/main/java/com/example/beam4/SelectPhotoActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.Toolbar;
+
 public class SelectPhotoActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_photo);
+
+        /*
+        toolbar=findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+         */
     }
 }
+
