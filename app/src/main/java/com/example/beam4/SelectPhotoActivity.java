013c74b57682981afae33ed0bfb6c49285fb92f4
jinsuSang <twtz0309@naver.com>
2020-04-10 14:14:05 +0900

    Add onItemClickListener to SelectPhotoActivityRowAdapter

--- app/src/main/java/com/example/beam4/SelectPhotoActivity.java
+++ app/src/main/java/com/example/beam4/SelectPhotoActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -11,7 +13,7 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.lang.reflect.Array;
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class SelectPhotoActivity extends AppCompatActivity {
@@ -19,7 +21,9 @@ public class SelectPhotoActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private ArrayList<Integer> photoGroup = new ArrayList<>();
     private ImageView bigImage;
-
+    private ImageButton checkButton;
+    private Button deleteExceptBM;
+    private ArrayList<Integer> selectedPhotoGroup = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +46,9 @@ public class SelectPhotoActivity extends AppCompatActivity {
             photoGroup.add(photoId[i]);
         }
 
+        bigImage = findViewById(R.id.bigImage);
+        bigImage.setImageResource(photoGroup.get(0));
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.select_photo_group);
         recyclerView.setLayoutManager(linearLayoutManager);
@@ -56,6 +63,32 @@ public class SelectPhotoActivity extends AppCompatActivity {
 
         recyclerView.setAdapter(adapter);
         recyclerView.addItemDecoration(new SelectPhotoActivityRowDecoration(25,25,30,30));
+
+        // checkButtonClick
+        CheckButtonClickListener checkButtonClickListener = new CheckButtonClickListener();
+        checkButton = findViewById(R.id.checkButton);
+        checkButton.setOnClickListener(checkButtonClickListener);
+
+//        // deleteExceptBM
+//        DeleteExceptBMListener deleteExceptBMListener =  new DeleteExceptBMListener()
+//        deleteExceptBM = findViewById(R.id.deleteExceptBM);
+//        deleteExceptBM.setOnClickListener(deleteExceptBMListener);
     }
+
+    class CheckButtonClickListener implements Button.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            selectedPhotoGroup.add(bigImage.getId());
+        }
+    }
+
+//    class DeleteExceptBMListener implements Button.OnClickListener{
+//
+//        @Override
+//        public void onClick(View v) {
+//            sendBroadcast();
+//        }
+//    }
 }
 
